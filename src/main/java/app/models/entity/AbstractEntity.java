e746a0eda2d9b7119dd746c366d916ec28371210
kunalkatiyarr <kunalkatiyar916@gmail.com>
2026-05-26 17:06:00 +0530

    refactor: modify AbstractEntity to support JPA Auditing and rename fields

--- src/main/java/app/models/entity/AbstractEntity.java
+++ src/main/java/app/models/entity/AbstractEntity.java
@@ -1,65 +1,72 @@
 package app.models.entity;
 
 import java.time.Instant;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class AbstractEntity {
 
     @GeneratedValue
     @Id
     private Long id;
 
-    private Instant creationTime;
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
 
-    private Instant lastModified;
-
-    @PrePersist
-    void createdAt() {
-
-        final Instant i = Instant.now();
-        creationTime = i;
-        lastModified = i;
-    }
-
-    @PreUpdate
-    void lastModified() {
-
-        setLastModified(Instant.now());
-    }
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Instant updatedAt;
 
     public Long getId() {
-
         return id;
     }
 
     public void setId(final Long id) {
-
         this.id = id;
     }
 
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(final Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(final Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Deprecated
     public Instant getCreationTime() {
-
-        return creationTime;
+        return createdAt;
     }
 
+    @Deprecated
     public void setCreationTime(final Instant creationTime) {
-
-        this.creationTime = creationTime;
+        this.createdAt = creationTime;
     }
 
+    @Deprecated
     public Instant getLastModified() {
-
-        return lastModified;
+        return updatedAt;
     }
 
+    @Deprecated
     public void setLastModified(final Instant lastModified) {
-
-        this.lastModified = lastModified;
+        this.updatedAt = lastModified;
     }
-
 }
