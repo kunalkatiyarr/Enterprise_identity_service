2fe7a248d08fa9049d9783fa6f94616b0b13e0dc
kunalkatiyarr <kunalkatiyar916@gmail.com>
2026-06-05 14:02:00 +0530

    refactor: extend JpaRepository and JpaSpecificationExecutor in UserRepository

--- src/main/java/app/repositories/UserRepository.java
+++ src/main/java/app/repositories/UserRepository.java
@@ -1,15 +1,16 @@
 package app.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import app.models.entity.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
     User findOneByUserName(String name);
 
