948e147c2380e5769016eef88882bb5bd544b16d
kunalkatiyarr <kunalkatiyar916@gmail.com>
2026-07-09 18:23:00 +0530

    security: permit Swagger and registration endpoints in WebSecurity

--- src/main/java/app/adapters/ApplicationSecurityAdapter.java
+++ src/main/java/app/adapters/ApplicationSecurityAdapter.java
@@ -2,6 +2,7 @@ package app.adapters;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -38,6 +39,8 @@ public class ApplicationSecurityAdapter extends WebSecurityConfigurerAdapter {
         .antMatchers("/css/**").permitAll()
         .antMatchers("/js/**").permitAll()
         .antMatchers("/images/**").permitAll()
+        .antMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+        .antMatchers(HttpMethod.POST, "/api/users").permitAll()
         .anyRequest().authenticated()
         .and()
         .formLogin().loginPage("/login").failureUrl("/login?error").permitAll()
